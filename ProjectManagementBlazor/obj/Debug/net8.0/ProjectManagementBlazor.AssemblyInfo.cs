1cb9dfde57683ee90eb8e7f599241a3d217f537b
sciiphoz <djiproduccer@gmail.com>
2026-05-04 10:19:40 +0300

    loading fixed

--- ProjectManagementBlazor/obj/Debug/net8.0/ProjectManagementBlazor.AssemblyInfo.cs
+++ ProjectManagementBlazor/obj/Debug/net8.0/ProjectManagementBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManagementBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecb668fba40860ab6b7f702e7b4a9db5ca05caa1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2016b16716d3526f0aa0503786164b23426865b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManagementBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManagementBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
