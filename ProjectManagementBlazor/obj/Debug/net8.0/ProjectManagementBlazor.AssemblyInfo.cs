6ae70403660af80782f8692032d4415fba11aecd
sciiphoz <djiproduccer@gmail.com>
2026-06-22 12:13:22 +0300

    asd

--- ProjectManagementBlazor/obj/Debug/net8.0/ProjectManagementBlazor.AssemblyInfo.cs
+++ ProjectManagementBlazor/obj/Debug/net8.0/ProjectManagementBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManagementBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5fde8bbc3b2a44cf4ae8265c0be8bbf41614032")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f3ab62b8027ca5a022cdb80602edd4d46990de2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManagementBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManagementBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
